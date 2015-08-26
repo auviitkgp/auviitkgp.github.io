caf39f915a00c198d52c1498d9a334bda60e1458
ManviG <manvigarg1995@gmail.com>
2015-08-26 21:57:38 +0530

    All recommended changes are done

--- resources/footer.php
+++
@@ -1,8 +0,0 @@
-<hr>
-<div id="footer" class="col-md-12">
-    <footer class="footer container-fluid text-center">
-        <p class="text-muted credit">
-            © 2015 IIT Kharagpur - AUV Team
-        </p>
-    </footer>
-</div>
